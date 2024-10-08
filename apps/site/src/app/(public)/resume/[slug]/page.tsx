13ca10d3b54c1e8f412a0b49fb449987517fce16
mostafizur <coder.mr11@gmail.com>
2024-10-08 13:04:30 +0600

    candidate details page seo data added

--- apps/site/src/app/(public)/resume/[slug]/page.tsx
+++ apps/site/src/app/(public)/resume/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { StrapiSeoFormate } from "@/lib/strapiSeo"
 import { Metadata, ResolvingMetadata } from "next"
 import Script from "next/script"
 import { CandidateProfile } from "@padma/metajob-ui"
+import { getLanguageFromCookie } from "@/utils/language"
 
 // *** generate metadata type
 type Props = {
@@ -14,33 +15,49 @@ type Props = {
 // *** generate metadata for the page
 export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
    const pageSlug = params?.slug
-   //const language = getLanguageFromCookie();
-   // fetch data
+   const language = getLanguageFromCookie()
+
+   // *** fetch seo data
    const { data } = await find(
-      "api/candidates",
+      "api/resumes",
       {
          filters: {
             slug: {
                $eq: pageSlug
             }
          },
-         populate: "deep",
+         populate: {
+            seo: {
+               fields: [
+                  "metaTitle",
+                  "metaDescription",
+                  "metaImage",
+                  "metaSocial",
+                  "keywords",
+                  "metaRobots",
+                  "structuredData",
+                  "metaViewport",
+                  "canonicalURL"
+               ]
+            }
+         },
          publicationState: "live",
-         locale: ["en"]
+         locale: language ? [language] : ["en"]
       },
       "no-cache"
    )
 
-   // if data?.data?.attributes?.seo is not available, return default data
-   if (!data?.data[0]?.attributes?.seo) {
+   // if seo is not available, return default data
+   if (!data?.data?.[0]?.attributes?.seo) {
       return {
          title: data?.data[0]?.attributes?.title || "Title not found",
          description: data?.data[0]?.attributes?.description || "Description not found"
       }
    }
 
-   return StrapiSeoFormate(data?.data[0]?.attributes?.seo, `/candidates/${pageSlug}`)
+   return StrapiSeoFormate(data?.data?.[0]?.attributes?.seo, `/resume/${pageSlug}`)
 }
+
 export default async function page({ params }: { params: { slug: string } }) {
    const pageSlug = params?.slug
 
