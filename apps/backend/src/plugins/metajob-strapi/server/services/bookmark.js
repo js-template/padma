0a24bf537e1845ad3e358e019559b613387edb3e
Zelal Hossain <zelal@ojjomedia.com>
2024-10-13 02:21:37 +0600

    Update bookmark.js

--- apps/backend/src/plugins/metajob-strapi/server/services/bookmark.js
+++ apps/backend/src/plugins/metajob-strapi/server/services/bookmark.js
@@ -1,8 +1,3 @@
 "use strict";
 const { createCoreService } = require("@strapi/strapi").factories;
-
-/**
- * bookmark service
- */
-
 module.exports = createCoreService("plugin::metajob-strapi.bookmark");
