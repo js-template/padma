1ba302c059587f7f63acdafa76cf3511a7041c35
Zelal Hossain <zelal@ojjomedia.com>
2024-10-14 13:51:58 +0600

    read me updated

--- apps/backend/src/plugins/metajob-strapi/server/services/my-service.js
+++ apps/backend/src/plugins/metajob-strapi/server/services/my-service.js
@@ -3,7 +3,7 @@
 module.exports = ({ strapi }) => ({
   async getWelcomeMessage() {
     const entry = await strapi.db.query("api::job.job").count();
-    console.log("ENtry", entry);
+    //console.log("ENtry", entry);
 
     return {
       totalJobs: entry,
