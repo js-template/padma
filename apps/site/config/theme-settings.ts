ac5067257a270de0568be625c95e3cafcdfe7dda
Zelal Hossain <zelal@ojjomedia.com>
2024-10-17 13:59:49 +0600

    Create theme-settings.ts

---
+++ apps/site/config/theme-settings.ts
@@ -0,0 +1,5 @@
+import { blockComponentMapping } from "@padma/metajob-ui" // Adjust the path to your theme package
+
+console.log("blockComponentMapping", blockComponentMapping)
+// Export the themeComponentMapping for site-wide usage
+export { blockComponentMapping }
