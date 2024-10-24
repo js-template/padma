1f658c6f8706c1ce5395bff8f6369f107146a777
mostafizur <coder.mr11@gmail.com>
2024-10-24 19:26:17 +0600

    blog details page dynamic component render added

--- apps/site/src/app/(public)/blog/[slug]/page.tsx
+++ apps/site/src/app/(public)/blog/[slug]/page.tsx
@@ -1,12 +1,10 @@
-import React from "react"
+import React, { Fragment } from "react"
 import { notFound } from "next/navigation"
 import { find } from "@/lib/strapi"
 import { StrapiSeoFormate } from "@/lib/strapiSeo"
 import { getLanguageFromCookie } from "@/utils/language"
 import { Metadata, ResolvingMetadata } from "next"
-import Script from "next/script"
-// FIXME: should be from theme settings
-import { BlogDetails } from "@padma/metajob-ui"
+import { loadActiveTheme } from "config/theme-loader"
 
 // *** generate metadata type
 type Props = {
@@ -15,6 +13,8 @@ type Props = {
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
+   const { getPublicComponents } = await loadActiveTheme()
+
    const pageSlug = params?.slug
 
    // redirect to 404 page if no pageSlug found
@@ -24,8 +24,8 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
    const language = getLanguageFromCookie()
 
-   // *** get blogs data from strapi ***
-   const { data, error } = await find(
+   // *** get blog-details data from strapi ***
+   const { data: detailsData, error } = await find(
       "api/posts",
       {
          filters: {
@@ -39,28 +39,9 @@ export default async function Page({ params }: { params: { slug: string } }) {
       },
       "force-cache"
    )
+   const pageDetailsData = detailsData?.data?.[0]
 
-   // *** get recent blogs data from strapi ***
-   const { data: recentBlogs, error: recentBlogsError } = await find(
-      "api/posts",
-      {
-         populate: {
-            featuredImage: {
-               fields: ["url"]
-            }
-         },
-         fields: ["title", "slug", "publishedAt"], // Fields to include in the response
-         pagination: {
-            pageSize: 3, //fetch 3 recent job
-            page: 1
-         },
-         publicationState: "live",
-         locale: language ? [language] : ["en"]
-      },
-      "force-cache"
-   )
-
-   // *** get  blogs-details page data from strapi ***
+   // *** get  blogs-details-page data from strapi ***
    const { data: blogPageData, error: blogPageError } = await find(
       "api/blog-detail",
       {
@@ -70,52 +51,26 @@ export default async function Page({ params }: { params: { slug: string } }) {
       "no-store"
    )
 
-   // *** get  blogs-category data from strapi ***
-   const { data: blogCategoryData, error: blogCategoryError } = await find(
-      "api/categories",
-      {
-         populate: {
-            image: {
-               fields: ["url"]
-            },
-            posts: {
-               count: true
-            }
-         },
-         fields: ["title", "slug"],
-         pagination: {
-            pageSize: 10, //fetch 10 blog-categories
-            page: 1
-         },
-         locale: language ? [language] : ["en"]
-      },
-      "no-store"
-   )
-
-   if (error) {
-      return <div>Something went wrong</div>
-   }
+   // if (error) {
+   //    return <div>Something went wrong</div>
+   // }
+   const blocks = blogPageData?.data?.attributes?.blocks || []
 
    return (
-      <>
-         {/* TODO: Check why do need to map */}
-         <BlogDetails
-            data={data?.data}
-            recentBlogs={recentBlogs?.data}
-            blogPageData={blogPageData?.data?.attributes}
-            blogCategories={blogCategoryData?.data}
-            language={language}
-         />
-         {/* {data?.data[0]?.attributes?.seo?.structuredData && (
-            <Script
-               id='json-ld-structured-data'
-               type='application/ld+json'
-               dangerouslySetInnerHTML={{
-                  __html: JSON.stringify(data?.data[0]?.attributes?.seo?.structuredData)
-               }}
-            />
-         )} */}
-      </>
+      <Fragment>
+         {/* Render the components dynamically using blockComponentMapping */}
+         {blocks?.map((block: any, index: number) => {
+            // @ts-ignore
+            const BlockConfig = getPublicComponents[block.__component]
+
+            if (BlockConfig) {
+               const { component: ComponentToRender } = BlockConfig
+
+               return <ComponentToRender key={index} block={block} data={pageDetailsData} language={language} />
+            }
+            return null // Handle missing component mapping case
+         })}
+      </Fragment>
    )
 }
 
