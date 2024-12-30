2553deab3b32a4ae435d1aa7ecb97cf1ab2c0d67
Zelal Hossain <zelal@ojjomedia.com>
2024-12-30 10:26:50 +0600

    project folder restructure

--- projects/river-theme/src/utils/placeholder.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import React from "react"
-
-const PlaceholderComponent: React.FC = () => {
-   return <div>This component is not yet available.</div>
-}
-
-export default PlaceholderComponent
