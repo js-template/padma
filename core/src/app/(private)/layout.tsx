4a137019c7670f8ffb7cb123610df302872707ab
mostafizur <86697071+mostafizur99@users.noreply.github.com>
2025-01-27 20:26:48 +0600

    code private layout updated

--- core/src/app/(private)/layout.tsx
+++ core/src/app/(private)/layout.tsx
@@ -1,30 +1,25 @@
-// TODO: Private layout api is available yet
-import React from "react"
-import PrivateLayoutHeader from "./header"
-import { find } from "@/lib/strapi"
-import { loadActiveTheme } from "config/theme-loader"
-import { getLanguageFromCookie } from "@/utils/language"
+import React, { Suspense } from "react"
 import { Box } from "@mui/material"
+import PrivateLayoutHeader from "./header"
 import PrivateLayoutFooter from "./footer"
+import { PrivateHeaderLoader } from "@/components/loader/private-head-loader"
 
 export default async function PrivateLayout({ children }: { children: React.ReactNode }) {
-   const activeTheme = await loadActiveTheme()
-
-   // Define as an empty object by default
-   // let getPublicComponents: Record<string, any> = {}
-
-   // if (activeTheme) {
-   //    getPublicComponents = activeTheme.getPublicComponents
-   //    // console.log(getPublicComponents)
-   // } else {
-   //    console.error("Active theme could not be loaded!")
-   // }
-
    return (
       <>
-         <PrivateLayoutHeader />
-         <main>{children}</main>
-         <PrivateLayoutFooter />
+         <Box sx={{ display: "flex" }}>
+            {/* private header */}
+            <Suspense fallback={<PrivateHeaderLoader />}>
+               <PrivateLayoutHeader />
+            </Suspense>
+            <Box sx={{ width: "100%", mt: "60px" }}>
+               <Box component='main' sx={{ flexGrow: 1, p: 3, pb: 0 }}>
+                  {children}
+               </Box>
+               {/* private footer */}
+               <PrivateLayoutFooter />
+            </Box>
+         </Box>
       </>
    )
 }
