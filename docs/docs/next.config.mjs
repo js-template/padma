bee00b21403616ee1296c49813b848e60eceb514
Zelal Hossain <zelal@ojjomedia.com>
2025-11-12 12:35:35 +0600

    Update next.config.mjs

--- docs/docs/next.config.mjs
+++ docs/docs/next.config.mjs
@@ -1,31 +1,27 @@
 // @ts-ignore
-import nextra from 'nextra'
+import nextra from "nextra";
 
 const withNextra = nextra({
-	theme: 'nextra-theme-docs',
-	themeConfig: './theme.config.tsx',
-	staticImage: true,
-	latex: true,
-	flexsearch: {
-		codeblocks: true,
-	},
-	defaultShowCopyCode: true
-	
-
-})
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  staticImage: true,
+  latex: true,
+  flexsearch: {
+    codeblocks: true,
+  },
+  defaultShowCopyCode: true,
+});
 
 export default withNextra({
-	reactStrictMode: true,
-	// eslint: {
-	// 	// Eslint behaves weirdly in this monorepo.
-	// 	ignoreDuringBuilds: true,
-	// },
-		images: {
-		unoptimized: true,
-	  },
-	  reactStrictMode: true,
-	  swcMinify: true,
-	  trailingSlash: true,
-	  output: "export",
-})
-
+  reactStrictMode: true,
+  // eslint: {
+  // 	// Eslint behaves weirdly in this monorepo.
+  // 	ignoreDuringBuilds: true,
+  // },
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+});
